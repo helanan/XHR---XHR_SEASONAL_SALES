0cfd2c7ed373e8418462b6f06984ebca3d3eb546
Helana <helanan@gmail.com>
2017-02-03 01:04:09 -0600

    added javascript

---
+++ Javascripts/seasonal.js
@@ -0,0 +1,30 @@
+// function getProducts () {
+// var data = JSON.parse(this.responseText);
+// var products = data.products;
+// var productsString = products.map(function(product) {
+// 	products.find(function(){
+// 		if (products.id === categories.id) {
+// 			var department = categories.name;
+// 		}
+// 	});
+// 	return `<div>${product.name}: ${product.price}</div>`;
+// 	}).join("");
+// 	container.innerHTML += productsString;
+// }
+
+// function getCategories () {
+//  var data = JSON.parse(this.response.Text);
+//  console.log(data);
+//  var categories = JSON.parse(this.responseText);
+// 	console.log(data);
+// var categories = data.categories;
+// 	console.log(categories)
+// }
+
+// function executeThisCodeIfXHRFails (){
+// 	console.log("An error occured while transferring");
+// }
+
+// function executeThisCodeIfXHRFails () {
+// 	console.log("An error occurred while transferring");
+// }
